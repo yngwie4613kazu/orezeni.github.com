a5052d5f54c6d2260efc6950f10bd4bc265b3b4a
yngwie4613kazu <yngwie4613kazu@gmail.com>
2013-12-14 18:33:57 +0900

    id2
    id2

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yngwie4613kazu';
+OpenSpending.identifier = 'urayasu_2013_yosan';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
